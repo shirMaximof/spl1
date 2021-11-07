73f673168a8caba7ff79dd8904d2fc55b9611e24
shirMaximof <maximof@post.bgu.ac.il>
2021-11-07 18:33:59 +0200

    new update

--- src/Action.cpp
+++ src/Action.cpp
@@ -1,4 +1,102 @@
-//
-// Created by 97252 on 07/11/2021.
-//
+#include "Action.h"
 
+//BaseAction
+    BaseAction::BaseAction() { }
+    ActionStatus BaseAction::getStatus() const { }
+    void BaseAction::complete()
+    {}
+    void BaseAction::error(std::string errorMsg){}
+    std::string getErrorMsg() const {}
+
+//OpenTrainer
+    OpenTrainer::OpenTrainer(int id, std::vector<Customer *> &customersList) : trainerId(id), customers(customersList) {}
+    void OpenTrainer::act(Studio &studio) {}
+    std::string OpenTrainer::toString() const {}
+
+
+
+class Order : public BaseAction {
+public:
+    Order(int id);
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+    const int trainerId;
+};
+
+
+class MoveCustomer : public BaseAction {
+public:
+    MoveCustomer(int src, int dst, int customerId);
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+    const int srcTrainer;
+    const int dstTrainer;
+    const int id;
+};
+
+
+class Close : public BaseAction {
+public:
+    Close(int id);
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+    const int trainerId;
+};
+
+
+class CloseAll : public BaseAction {
+public:
+    CloseAll();
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+};
+
+
+class PrintWorkoutOptions : public BaseAction {
+public:
+    PrintWorkoutOptions();
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+};
+
+
+class PrintTrainerStatus : public BaseAction {
+public:
+    PrintTrainerStatus(int id);
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+    const int trainerId;
+};
+
+
+class PrintActionsLog : public BaseAction {
+public:
+    PrintActionsLog();
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+};
+
+
+class BackupStudio : public BaseAction {
+public:
+    BackupStudio();
+    void act(Studio &studio);
+    std::string toString() const;
+private:
+};
+
+
+class RestoreStudio : public BaseAction {
+public:
+    RestoreStudio();
+    void act(Studio &studio);
+    std::string toString() const;
+
+};
